b23a1390755dccbf369ff700cd468cba805f52ee
Ronnie Villarini <ronnie@ronini.dev>
2019-05-28 13:31:19 -0500

    add routing

---
+++ app/components/app/index.js
@@ -0,0 +1,46 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+import Popular from './components/Popular';
+import Battle from './components/Battle';
+import Nav from './components/Nav';
+import Results from './components/Results';
+import { ThemeProvider } from './context/theme';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      theme: 'light',
+      toggleTheme: () => {
+        this.setState(({ theme }) => ({
+          theme: theme === 'light' ? 'dark' : 'light',
+        }));
+      },
+    };
+  }
+
+  render() {
+    return (
+      <Router>
+        <ThemeProvider value={this.state}>
+          <div className={this.state.theme}>
+            <div className="container">
+              <Nav />
+              <Switch>
+                <Route path="/" exact component={Popular} />
+                <Route path="/battle" exact component={Battle} />
+                <Route path="/battle/results" component={Results} />
+                <Route render={() => <h1>404</h1>} />
+              </Switch>
+            </div>
+          </div>
+        </ThemeProvider>
+      </Router>
+    );
+  }
+}
+
+ReactDOM.render(<App />, document.getElementById('app'));
