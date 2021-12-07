1fd505654a638fb4f06533d3fb6c1efa9aee3f1e
Ronini <ronnie@ronini.dev>
2021-12-07 16:00:18 -0600

    add click handler to nav

--- app/components/Popular.jsx
+++ app/components/Popular.jsx
@@ -1,13 +1,40 @@
 import React from "react";
 
 export default class Popular extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      selectedLanguage: "All",
+    };
+  }
+
+  updateSelectedLanguage = (language) => {
+    this.setState({
+      selectedLanguage: language,
+    });
+  };
+
   render() {
     const languages = ["All", "JavaScript", "Ruby", "Java", "CSS", "Python"];
     return (
       <ul className="flex-center">
         {languages.map((language) => (
-          <li key={language}>
-            <button className="btn-clear nav-link">{language}</button>
+          <li
+            key={language}
+            style={{
+              color:
+                this.state.selectedLanguage === language
+                  ? "hsl(264.7, 100%, 46.7%)"
+                  : "",
+            }}
+          >
+            <button
+              className="btn-clear nav-link"
+              onClick={() => this.updateSelectedLanguage(language)}
+            >
+              {language}
+            </button>
           </li>
         ))}
       </ul>
